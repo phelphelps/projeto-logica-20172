5a987f6aafb44f51095bc9fdb59381c425347b62
Emerson Victor <evfl@cin.ufpe.br>
2017-12-08 16:52:45 -0300

    Start project

--- APLV-system/APLV-system.pl
+++ APLV-system/APLV-system.pl
@@ -1,138 +1,67 @@
-% Library for graphic interface
-
-:-use_module(library(pce)).
-:-use_module(library(pce_style_item)).
-
 /*
+true(sim).
+homem(homem).
 
-Tempo de reação
-	Se for menor que uma hora
-		Se tiver sintomas fortes recomendar aquele coisa laranja
-		
-		Se não
-			Testar os sintomas:
-				Se amamentar:
-					Breastfed
-					Exclude cow’s milk
-					containing foods from
-					maternal diet for 2-4
-					weeks.
-					Do not home
-					challenge after 2-4
-					weeks if an
-					improvement.
-					Prescribe for
-					mother:
-					Calcium carbonate
-					1.25g and
-					cholecalciferol 10mcg
-					chewable tablets -
-					2 daily
+caixaSimNao(Pergunta, Titulo) :-
+	new(Interface, dialog(Titulo)),
 
-				(ETAPA X) Se não:
-					Formula fed and
-					mixed
-					Prescribe EHF.
-					Try for min 2
-					weeks.
-					Initially prescribe
-					2-3 tins (week
-					supply) until
-					tolerance reached.
-					Practical advice -
-					page 6
+	new(Resposta, menu(Pergunta)),
+	send_list(Resposta, append, [sim, nao]),
 
-					If improvement do not
-					home challenge and
-					continue with EHF.
-					Quantities to
-					prescribe – page 5
+	new(Botao, button('Continuar', message(Interface, return, Resposta?selection))),
 
-					If no improvement
-					If infant on EHF and
-					CMPA still suspected
-					prescribe AAF.
-					Ensure practical advice
-					followed before switch
-					(see page 6).
-
-
-	Se for maior testar os sintomas
-		Ver se tem sinal de faltering growth
-			Se sim:
-				Se mamar:
-					Exclusively breastfed
-					(exclude breastfeeding
-					technique issues first)
-					Exclude cow’s milk
-					containing foods from
-					maternal diet for 2-4
-					weeks.
-					Prescribe for mother:
-					Calcium carbonate 1.25g
-					and cholecalciferol 10mcg
-					chewable tablets - 2 daily
-					Challenge with normal
-					maternal diet after 2-4
-					weeks to confirm
-					diagnosis.
-					If symptoms return
-					continue maternal cow’s
-					milk free diet till review by
-					dietitian (if applicable).
-
-				Se não: seguir ETAPA X
-
-			Se não:
-			Formula fed and mixed
-			Prescribe EHF.
-			Try for min 2 weeks.
-			Initially prescribe 2-3 tins
-			(week supply) until
-			tolerance reached.
-			Product choice - page 5
-			Practical advice - page 6
-
-				If improvement:
-				Perform home challenge
-				to confirm diagnosis,
-				2- 4 weeks after starting
-				EHF (click here for
-				example of home
-				challenge4
-				).
-				If symptoms return
-				continue with EHF.
-				Quantities to prescribe
-				– page 5
-				If symptoms do not return
-				with challenge CMPA is
-				ruled out.
-				If no improvement:
-				If infant on EHF and
-				CMPA still suspected
-				prescribe AAF.
-				Ensure practical advice
-				followed before switch
-				(see page 6).
-
-
-
-
-
-
-All infants with CMPA should be seen by paediatric dietitian to ensure optimal
-nutrition. Referral criteria and access to community and specialist support
-vary across Greater Manchester. Follow local pathway.
-
-Product choice and quantities to prescribe – see page 5
-Practical advice – see page 6
-Prescription management – see page 6
-Re-challenging – see page 7
-
+	send_list(Interface, append, [Resposta, Botao]),
+	get(Interface, confirm, Resposta1),
+	free(Interface),
+	true(Resposta1).
 */
 
-/* 
+% List of symptoms
+delayedSym :- 
+	write('The pacient present one or more of these symptoms?\n\n. Severe colic\n. Reflux-GORD\n. Food refusal or aversion\n. Perianal redness\n. Constipation\n. Abdominal discomfort\n. Blood/mucus in stools (in an otherwise well infant)\n. Pruritus, erythema\n. Significant atopic eczema').
+
+acuteOnsetSym :-
+	write('The pacient present one or more of these symptoms?\n\n. Vomiting\n. Diarrhoea\n. Abdominal pain/colic\n. Acute pruritus, erythema, urticaria\n. Angioedema\n. Acute ‘flare up’ of atopic eczema').
+
+faltheringGrowth :-
+	write('The paciente present signs of faltering growth?').
+
+% Time of symptoms
+timeSym :-
+	write('Write the time(hours) that onset of symptoms after ingestion of cow’s milk protein'),
+	nl,
+	read(Input),
+	acuteOnset(Input).
+
+% Acute onset symptoms diagnostic
+acuteOnset(Time):- Time > 1, delayedSym; acuteOnsetSym.
 
 
-*/
+
+
+% Main
+diag :- true; send(@display, inform, 'O paciente diagnosticado nao possui anemia.').
+
+% Graphic Interface
+
+:-	pce_image_directory('./').
+	resource(imagem, image, image('apvl.jpeg')).
+
+:-  	new(Interface, dialog('DIAGNOSTICO DE ANEMIA')),
+	new(Menu, menu_bar),
+	send(Menu, append, new(Consultar, popup(menu))),
+
+	% OPCOES DO MENU
+
+	send_list(Consultar, append, [menu_item('Autores', message(@display, inform,
+	"Desenvolvido por:\n- Emerson Victor\n- Ewerton Felipe\n- José Claudino\n- José Tomáx\n\n(C) 2017.")), menu_item(sair, and(message(Interface, destroy), message(Interface, free)))]),
+
+	new(Imagem, label(nome, resource(imagem))),
+	new(Titulo, text('CMPA Diagnostic System')),
+	new(Botao, button('Iniciar Consulta', and(message(@prolog, diag)))),
+
+	% FORMATANDO O TITULO DO PROGRAMA
+
+	send(Titulo, font, font(times, bold, 20)),
+	send_list(Interface, append, [Menu, Imagem, Titulo, Botao]),
+	send(Interface, open, point(300,300)).

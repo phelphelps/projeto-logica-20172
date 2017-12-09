b4caf3ec4ddd244da86c05a2d85b6cbc1a924fd9
phelpelps <effs@cin.ufpe.br>
2017-12-09 14:38:09 -0300

    Função print() adicionada (printa dentro da interface).

--- APLV-system/APLV-system.pl
+++ APLV-system/APLV-system.pl
@@ -16,19 +16,19 @@ checkButton(Question, Title) :-
 % Time of symptoms
 timeSym :-
 	
-    new(D, dialog('Number of hours after ingestion')),
-    send(D, append, new(label)),
-    send(D, append, new(Hours, text_item(hours))),
+    new(Interface, dialog('Number of hours after ingestion')),
+    send(Interface, append, new(label)),
+    send(Interface, append, new(Hours, text_item(hours))),
 
     send(Hours, type, int),
 
-    send(D, append,
+    send(Interface, append,
          button(create, message(@prolog, acuteOnset,
                                 Hours?selection
                                 ))),
 
-    send(D, default_button, create),
-    send(D, open).
+    send(Interface, default_button, create),
+    send(Interface, open).
 
 
 % Acute onset symptoms diagnostic
@@ -54,13 +54,13 @@ breastfedDelayed :-
 	checkButton('Is the baby exclusively breastfed?', 'DIAGNOSIS').
 	
 breastfedDelayedCheck :- 
-	breastfedDelayed -> write('(exclude breastfeeding technique issues first)\n\nExclude cows milk containing foods frommaternal diet for 2-4weeks.\nPrescribe for mother:\n\nCalcium carbonate 1.25g\nCholecalciferol 10mcg\nChewable tablets - 2 daily\n\nChallenge with normal maternal diet after 2-4 weeks to confirm diagnosis.\nIf symptoms return continue  maternal cows milk free diet till review by dietitian (if applicable).'); checkEhf.
+	breastfedDelayed -> print('(exclude breastfeeding technique issues first)\n\nExclude cows milk containing foods frommaternal diet for 2-4weeks.\nPrescribe for mother:\n\nCalcium carbonate 1.25g\nCholecalciferol 10mcg\nChewable tablets - 2 daily\n\nChallenge with normal maternal diet after 2-4 weeks to confirm diagnosis.\nIf symptoms return continue  maternal cows milk free diet till review by dietitian (if applicable).'); checkEhf.
 
 breastfedOnset :-
 	checkButton('Is the baby breastfed?', 'DIAGNOSIS').
 
 breastfedOnsetCheck :- 
-	not(breastfedOnset) -> write('Exclude cow’s milk containing foods from maternal diet for 2-4 weeks.
+	not(breastfedOnset) -> print('Exclude cow’s milk containing foods from maternal diet for 2-4 weeks.
 	\nDo not home challenge after 2-4 weeks if an improvement.\nPrescribe for mother:\n\nCalcium carbonate 1.25g\ncholecalciferol 10mcg
 	\nchewable tablets - 2 daily', 'DYAGNOSTIC'); checkEhf.
 
@@ -72,12 +72,12 @@ checkEhf :-
 ehfAge :-
 	checkButton('Is the pacient over 6 life months?','DIAGNOSIS').
 ehfPresc:-
-	not(ehfAge) -> write('Althera(450g)\nAptamil Pepiti 1(400/800g)\nNutramigen LGG 1 (400g)\nSimilac Alimentum (400g)') ; write('Aptamil Pepti 2 (400g/800g)\nNutramigen LGG 2 (400g)').
+	not(ehfAge) -> print('Althera(450g)\nAptamil Pepiti 1(400/800g)\nNutramigen LGG 1 (400g)\nSimilac Alimentum (400g)') ; print('Aptamil Pepti 2 (400g/800g)\nNutramigen LGG 2 (400g)').
 
 % Prescription - AAF
 
 aafPresc :-
-	write('Alfamino (400g)\nNeocate LCP(400g)\nNutramigen Puramino (400g)').
+	print('Alfamino (400g)\nNeocate LCP(400g)\nNutramigen Puramino (400g)').
 
 
 
@@ -89,13 +89,13 @@ improvementQuestion_1 :-
 	checkButton('Has there been improvement?','DIAGNOSIS').
 	
 improvementCheck_1 :-
-	not(improvementQuestion_1) -> (write('If infant on EHF and CMPA still suspected prescribe AAF.\nIt is the options:\n\n'),aafPresc); write('Perform home challenge to confirm diagnosis, 2- 4 weeks after starting EHF. If symptoms return continue with EHF.').
+	not(improvementQuestion_1) -> (print('If infant on EHF and CMPA still suspected prescribe AAF.\nIt is the options:\n\n'),aafPresc); print('Perform home challenge to confirm diagnosis, 2- 4 weeks after starting EHF. If symptoms return continue with EHF.').
 
 improvementQuestion_2 :-
 	checkButton('Has there been improvement?', 'DIAGNOSIS').
 	
 improvementCheck_2 :-
-	not(improvementQuestion_2) -> (write('If infant on EHF and CMPA still suspected prescribe AAF.\nIt is the options:\n\n'),aafPresc); write('If improvement do not home challenge and continue with EHF.').
+	not(improvementQuestion_2) -> (print('If infant on EHF and CMPA still suspected prescribe AAF.\nIt is the options:\n\n'),aafPresc); print('If improvement do not home challenge and continue with EHF.').
 
 % Main
 
@@ -103,7 +103,7 @@ start :-
 	timeSym.
 
 xdelayedSym :-
-	not(delayedSym) -> write('So, the pacient is fine.'); xfalteringGrowth.
+	not(delayedSym) -> print('So, the pacient is fine.'); xfalteringGrowth.
 
 xfalteringGrowth :-
 	not(falteringGrowth) -> breastfedDelayedCheck; breastfedDelayedCheck.
@@ -111,10 +111,15 @@ xfalteringGrowth :-
 xacuteOnsetSym :-
 	acuteOnsetSym_1 -> breastfedOnsetCheck; xacuteOnsetSym_2.
 xacuteOnsetSym_2 :-
-	acuteOnsetSym_2 -> (write('Urgently treat symptoms.Immediately refer to specialist.\nPrescribe AAF.\nIt is the options:\n\n'),aafPresc); write('So, the pacient is fine').
+	acuteOnsetSym_2 -> (print('Urgently treat symptoms.Immediately refer to specialist.\nPrescribe AAF.\nIt is the options:\n\n'),aafPresc); print('So, the pacient is fine').
 
 % Graphic Interface
-
+print(Text) :-
+        new(D, window('DIAGNOSIS')),
+        send(D, size, size(600, 600)),
+        new(T, text(Text)),
+        send(D, display, T, point(100, 100)),
+        send(D, open).
 :-	pce_image_directory('./').
 	resource(imagem, image, image('apvl.jpeg')).
 
@@ -122,7 +127,7 @@ xacuteOnsetSym_2 :-
 	new(Menu, menu_bar),
 	send(Menu, append, new(Consultar, popup(menu))),
 
-	% Menu options
+% Menu options
 
 	send_list(Consultar, append, [menu_item('Authors', message(@display, inform,
 	"Developed by:\n- Emerson Victor\n- Ewerton Felipe\n- José Claudino\n- José Tomáx\n\n(C) 2017.")), menu_item(sair, and(message(Interface, destroy), message(Interface, free)))]),
@@ -131,12 +136,13 @@ xacuteOnsetSym_2 :-
 	new(Title, text('CMPA Diagnostic System')),
 	new(Button, button('  Start diagnosis  ', and(message(@prolog, start)))),
 	
-	send(Interface, append, button(  postDiagnosis, message(@prolog, improvementButton))),
+	send(Interface, append, button('   Post-diagnosis   ', message(@prolog, improvementButton))),
 
-    send(Interface,default_button,  postDiagnosis),
+    send(Interface,default_button,'   Post-diagnosis   '),
 
 
-	% Formatting program title
+
+% Formatting program title
 
 	send(Title, font, font(times, bold, 20)),
 	send_list(Interface, append, [Menu, Imagem, Title, Button]),
